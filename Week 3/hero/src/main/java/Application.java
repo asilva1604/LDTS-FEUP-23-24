53982716ec8eca4e999ebed15db5b32f76094dc1
Alexandre Silva <asilva1604@outlook.com>
2023-09-28 17:47:29 +0100

    Created Application and Game class

--- Week 3/hero/src/main/java/Application.java
+++ Week 3/hero/src/main/java/Application.java
@@ -1,31 +1,6 @@
-import com.googlecode.lanterna.TerminalSize;
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.TerminalFactory;
-
-import java.io.IOException;
-
 public class Application {
     public static void main(String[] args) {
-        try {
-            TerminalSize terminalSize = new TerminalSize(40, 20);
-            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
-            Terminal terminal = new DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X') [0]);
-            screen.refresh();
-            // we don't need a cursor
-            // screens must be started
-            // resize screen if necessary
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Game game = new Game();
+        game.run();
     }
 }
---
+++ Week 3/hero/src/main/java/Game.java
@@ -0,0 +1,62 @@
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.input.KeyStroke;
+
+import java.io.IOException;
+import java.security.Key;
+
+public class Game {
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
+    public Game() {
+        try {
+            TerminalSize terminalSize = new TerminalSize(40, 20);
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
+            Terminal terminal = new DefaultTerminalFactory().createTerminal();
+            screen = new TerminalScreen(terminal);
+            screen.setCursorPosition(null);
+            screen.startScreen();
+            screen.doResizeIfNecessary();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void draw() throws IOException{
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X') [0]);
+        screen.refresh();
+    }
+
+    public void run(){
+        try {
+            while (true) {
+                draw();
+                KeyStroke key = screen.readInput();
+                processKey(key);
+                if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                    screen.close();
+                } else if (key.getKeyType() == KeyType.EOF) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void processKey(KeyStroke key) {
+        switch (key.getKeyType()) {
+            case ArrowLeft -> x--;
+            case ArrowRight -> x++;
+            case ArrowUp -> y--;
+            case ArrowDown -> y++;
+        }
+    }
+}
